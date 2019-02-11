1024cb123f1baa5edb666faaf38569c3c79c3556
andy.rothwell <rothwell.andy@gmail.com>
2019-02-11 17:00:03 -0500

    makes pvc work

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faInfoCircle } from '@fortawesome/pro-solid-svg-icons/faInfoCircle';
+import { faExternalLink } from '@fortawesome/pro-solid-svg-icons/faExternalLink';
+library.add(faInfoCircle, faExternalLink);
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 import '../node_modules/phila-standards/dist/css/phila-app.min.css';
 
 Vue.config.productionTip = false
--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,15 @@ module.exports = {
     plugins: [
       new Visualizer({ filename: './statistics.html' })
     ]
+  },
+  chainWebpack: (config) => {
+    // A, remove the plugin
+    config.plugins.delete('prefetch')
+    // or:
+    // B. Alter settings:
+    // config.plugin('prefetch').tap(options => {
+    //   options.fileBlackList.push([/myasyncRoute(.)+?\.js$/])
+    //   return options
+    // })
   }
 }
